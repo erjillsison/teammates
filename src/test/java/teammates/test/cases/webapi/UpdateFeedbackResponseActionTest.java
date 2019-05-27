275462f9a4b96846c1fb82448eb9b3212a77295b
erjillsison <erjillsison@gmail.com>
2019-05-27 16:55:21 +0800

    Add testAccessControl test cases

--- src/test/java/teammates/test/cases/webapi/UpdateFeedbackResponseActionTest.java
+++ src/test/java/teammates/test/cases/webapi/UpdateFeedbackResponseActionTest.java
@@ -2,7 +2,11 @@ package teammates.test.cases.webapi;
 
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
+import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
+import teammates.common.exception.EntityNotFoundException;
 import teammates.common.util.Const;
+import teammates.ui.webapi.action.Intent;
 import teammates.ui.webapi.action.UpdateFeedbackResponseAction;
 
 /**
@@ -29,7 +33,47 @@ public class UpdateFeedbackResponseActionTest extends BaseActionTest<UpdateFeedb
     @Test
     @Override
     protected void testAccessControl() throws Exception {
-        // TODO
+
+        ______TS("non-existent feedback response");
+
+        assertThrows(EntityNotFoundException.class, () -> {
+            getAction(new String[] {Const.ParamsNames.FEEDBACK_RESPONSE_ID, "randomId"}).checkSpecificAccessControl();
+        });
+
+        ______TS("instructor can not update student feedback response");
+
+        int questionNumber = 1;
+        FeedbackQuestionAttributes feedbackQuestion = logic.getFeedbackQuestion(
+                "First feedback session", "idOfTypicalCourse1", questionNumber);
+
+        String giverEmail = "student1InCourse1@gmail.tmt";
+        String receiverEmail = "student1InCourse1@gmail.tmt";
+        FeedbackResponseAttributes feedbackResponse = logic.getFeedbackResponse(feedbackQuestion.getId(),
+                giverEmail, receiverEmail);
+
+        String[] params = new String[] {
+                Const.ParamsNames.FEEDBACK_RESPONSE_ID, feedbackResponse.getId(),
+                Const.ParamsNames.INTENT, Intent.INSTRUCTOR_SUBMISSION.toString(),
+        };
+
+        verifyInaccessibleForInstructors(params);
+
+        ______TS("students can not access instructor feedback response");
+
+        questionNumber = 3;
+        feedbackQuestion = logic.getFeedbackQuestion(
+                "First feedback session", "idOfTypicalCourse1", questionNumber);
+        giverEmail = "instructor1@course1.tmt";
+        receiverEmail = "%GENERAL%";
+        feedbackResponse = logic.getFeedbackResponse(feedbackQuestion.getId(),
+                giverEmail, receiverEmail);
+
+        params = new String[] {
+                Const.ParamsNames.FEEDBACK_RESPONSE_ID, feedbackResponse.getId(),
+                Const.ParamsNames.INTENT, Intent.STUDENT_SUBMISSION.toString(),
+        };
+
+        verifyInaccessibleForStudents(params);
     }
 
 }
