75b421d4f72d19ef890e794ef6bf4b9bdfb58f05
erjillsison <erjillsison@gmail.com>
2019-05-28 15:31:50 +0800

    Update RestoreFeedbackSessionActionTest.java

--- src/test/java/teammates/test/cases/webapi/RestoreFeedbackSessionActionTest.java
+++ src/test/java/teammates/test/cases/webapi/RestoreFeedbackSessionActionTest.java
@@ -2,8 +2,13 @@ package teammates.test.cases.webapi;
 
 import org.testng.annotations.Test;
 
+import teammates.common.datatransfer.attributes.CourseAttributes;
+import teammates.common.datatransfer.attributes.FeedbackSessionAttributes;
+import teammates.common.exception.EntityNotFoundException;
 import teammates.common.util.Const;
+import teammates.ui.webapi.action.JsonResult;
 import teammates.ui.webapi.action.RestoreFeedbackSessionAction;
+import teammates.ui.webapi.output.FeedbackSessionData;
 
 /**
  * SUT: {@link RestoreFeedbackSessionAction}.
@@ -23,13 +28,45 @@ public class RestoreFeedbackSessionActionTest extends BaseActionTest<RestoreFeed
     @Test
     @Override
     protected void testExecute() throws Exception {
-        // TODO
+
+        ______TS("Failure case: Session is not in recycle bin");
+
+        CourseAttributes course = typicalBundle.courses.get("typicalCourse1");
+        FeedbackSessionAttributes fs = typicalBundle.feedbackSessions.get("session1InCourse1");
+
+        String[] params = new String[] {
+                Const.ParamsNames.COURSE_ID, course.getId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+        };
+
+        assertThrows(EntityNotFoundException.class, () -> getJsonResult(getAction(params)));
+
+        ______TS("Typical case");
+
+        logic.moveFeedbackSessionToRecycleBin(fs.getFeedbackSessionName(), course.getId());
+
+        RestoreFeedbackSessionAction restoreFeedbackSessionAction = getAction(params);
+        JsonResult result = getJsonResult(restoreFeedbackSessionAction);
+        FeedbackSessionData feedbackSessionData = (FeedbackSessionData) result.getOutput();
+
+        assertEquals(feedbackSessionData.getFeedbackSessionName(), fs.getFeedbackSessionName());
+        assertNull(feedbackSessionData.getDeletedAtTimestamp());
     }
 
     @Test
     @Override
     protected void testAccessControl() throws Exception {
-        // TODO
-    }
+        CourseAttributes course = typicalBundle.courses.get("typicalCourse1");
+        FeedbackSessionAttributes fs = typicalBundle.feedbackSessions.get("session1InCourse1");
 
+        String[] params = new String[] {
+                Const.ParamsNames.COURSE_ID, course.getId(),
+                Const.ParamsNames.FEEDBACK_SESSION_NAME, fs.getFeedbackSessionName(),
+        };
+
+        logic.moveFeedbackSessionToRecycleBin(fs.getFeedbackSessionName(), course.getId());
+
+        verifyOnlyInstructorsOfTheSameCourseCanAccess(params);
+        verifyInaccessibleWithoutModifySessionPrivilege(params);
+    }
 }
