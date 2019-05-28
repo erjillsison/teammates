2491885e4e2cca6a1a1d4e3378c0699983a3c961
erjillsison <erjillsison@gmail.com>
2019-05-28 10:13:19 +0800

    Add test cases in testExecute

--- src/test/java/teammates/test/cases/webapi/UpdateFeedbackResponseActionTest.java
+++ src/test/java/teammates/test/cases/webapi/UpdateFeedbackResponseActionTest.java
@@ -4,10 +4,12 @@ import org.testng.annotations.Test;
 
 import teammates.common.datatransfer.attributes.FeedbackQuestionAttributes;
 import teammates.common.datatransfer.attributes.FeedbackResponseAttributes;
+import teammates.common.datatransfer.attributes.StudentAttributes;
 import teammates.common.exception.EntityNotFoundException;
 import teammates.common.util.Const;
 import teammates.ui.webapi.action.Intent;
 import teammates.ui.webapi.action.UpdateFeedbackResponseAction;
+import teammates.ui.webapi.request.FeedbackResponseUpdateRequest;
 
 /**
  * SUT: {@link UpdateFeedbackResponseAction}.
@@ -27,7 +29,38 @@ public class UpdateFeedbackResponseActionTest extends BaseActionTest<UpdateFeedb
     @Test
     @Override
     protected void testExecute() throws Exception {
-        // TODO
+        ______TS("Unsuccessful case: not enough parameters");
+
+        verifyHttpParameterFailure();
+
+        ______TS("Successful updated feedback response");
+
+        int questionNumber = 1;
+        FeedbackQuestionAttributes feedbackQuestion = logic.getFeedbackQuestion(
+                "First feedback session", "idOfTypicalCourse1", questionNumber);
+
+        String giverEmail = "student1InCourse1@gmail.tmt";
+        String receiverEmail = "student1InCourse1@gmail.tmt";
+        FeedbackResponseAttributes feedbackResponse =
+                logic.getFeedbackResponse(feedbackQuestion.getId(), giverEmail, receiverEmail);
+
+        // Edit the feedback response
+        String[] submissionParams = new String[] {
+                Const.ParamsNames.FEEDBACK_RESPONSE_ID, feedbackResponse.getId(),
+                Const.ParamsNames.INTENT, Intent.STUDENT_SUBMISSION.toString(),
+                Const.ParamsNames.FEEDBACK_SESSION_MODERATED_PERSON, giverEmail,
+        };
+
+        FeedbackResponseUpdateRequest responseUpdateRequest = new FeedbackResponseUpdateRequest();
+
+        //TODO requestbody
+        // UpdateFeedbackResponseAction action = getAction(submissionParams);
+        //getJsonResult(action);
+        //FeedbackResponseAttributes fra = logic.getFeedbackResponse(feedbackResponse.getId());
+
+        // Compare before and after
+        // assertEquals(fra.getResponseDetails().getAnswerString(),feedbackResponse.getResponseDetails().getAnswerString());
+
     }
 
     @Test
